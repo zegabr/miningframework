fedbceb364db87a28035a17c711138e74c84f27b
zegabr <jgsp2@cin.ufpe.br>
2023-01-20 20:16:14 -0300

    add TODO

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 ./gradlew assemble
-rm -rf ~/Desktop/python_results/
+# TODO: replicate block below to create one folder for each repo, so that way we can run each repo in a isolated maner by commenting this code
+#
+rm -rf ~/Desktop/tensorflow_results/
 echo "results removed"
-./gradlew run --args="-e .py -i injectors.CSDiffModule -l '( ) : ,' -s 01/01/2021 -u 01/11/2021 ./python_projects.csv ~/Desktop/python_results"
-cat ~/Desktop/python_results/results.csv | grep false
+./gradlew run --args="-e .py -i injectors.CSDiffModule -l '( ) : ,' -s 01/01/2021 -u 01/11/2021 ./tensorflow.csv ~/Desktop/tensorflow_results"
+cat ~/Desktop/tensorflow_results/results.csv | grep false
