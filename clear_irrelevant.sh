bff78ab97e9f99a880769b016a3c1dc2e7ccd92f
zegabr <jgsp2@cin.ufpe.br>
2023-01-25 19:02:55 -0300

    update csdiff_v3 and utility scripts

--- clear_irrelevant.sh
+++ clear_irrelevant.sh
@@ -3,11 +3,17 @@ find . -type d -exec bash -c '
         if [ -z "$(find "$d" -mindepth 1 -type d)" ]; then
             if [ ! -e "$d/csdiff.py" ]; then
                 rm -r "$d"
+            elif [ -e "$d/csdiff.py" -a -e "$d/merge.py" ] ; then
+                cmp -s "$d/csdiff.py" "$d/merge.py"
+                if [ $? -eq 0 ]; then
+                    rm -r "$d"
+                fi
             fi
         fi
     done
 ' bash {} +
 
 find . -type f -name "results.csv" -delete
+find . -type f -name "skipped-merge-commits.csv" -delete
 
 find . -type d -empty -delete -not -path "*/\.*"
