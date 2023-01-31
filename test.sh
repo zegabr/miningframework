baad9f0daa9656648a1ee4ed38ad6f01197b98ba
zegabr <jgsp2@cin.ufpe.br>
2023-01-31 17:21:17 -0300

    make test reproducible from any computer

--- test.sh
+++ test.sh
@@ -2,7 +2,9 @@
 
 ./gradlew assemble
 
-names=(matplotlib tensorflow certbot flask ipython requests salt scrapy sentry tornado)
+names=(matplotlib)
+miningframework_path=$(pwd)
+results_path="$miningframework_path/bug_results"
 
 get_relevant_csv(){
     # Store the output of ls -1 in an array
@@ -17,30 +19,31 @@ get_relevant_csv(){
     done
 
     # Add the file path
-    command="$command /home/ze/Desktop/mining_results/all_results.csv"
+    command="$command "$results_path"/all_results.csv"
 
     # Run the command
     eval $command
 }
 
+
 for i in "${names[@]}"
 do
-  rm -rf /home/ze/Desktop/mining_results/${i}_results/
-  ./gradlew run --args="-e .py -i injectors.CSDiffModule -l '( ) : ,' -s 01/01/2021 -u 01/01/2022 ./projects/${i}.csv /home/ze/Desktop/mining_results/${i}_results"
+  rm -rf "$results_path"/${i}_results/
+  ./gradlew run --args="-e .py -i injectors.CSDiffModule -l '( ) : ,' -s 01/06/2021 -u 01/01/2022 ./projects/${i}.csv "$results_path"/${i}_results"
 
   if [[ "$i" == "${names[0]}" ]]; then
-    head -n 1 /home/ze/Desktop/mining_results/${i}_results/results.csv > /home/ze/Desktop/mining_results/relevant.csv
-    head -n 1 /home/ze/Desktop/mining_results/${i}_results/results.csv > /home/ze/Desktop/mining_results/all_results.csv
+    head -n 1 "$results_path"/${i}_results/results.csv > "$results_path"/relevant.csv
+    head -n 1 "$results_path"/${i}_results/results.csv > "$results_path"/all_results.csv
   fi
-    grep -e "false" /home/ze/Desktop/mining_results/${i}_results/results.csv  >> /home/ze/Desktop/mining_results/relevant.csv
-    grep -e "true" -e "false" /home/ze/Desktop/mining_results/${i}_results/results.csv >> /home/ze/Desktop/mining_results/all_results.csv
+    grep -e "false" "$results_path"/${i}_results/results.csv  >> "$results_path"/relevant.csv
+    grep -e "true" -e "false" "$results_path"/${i}_results/results.csv >> "$results_path"/all_results.csv
   # clearing miningframework mistakes
-  cd /home/ze/Desktop/mining_results/${i}_results/
-  bash /home/ze/miningframework/clear_irrelevant.sh
+  cd "$results_path"/${i}_results/
+  bash "$miningframework_path"/clear_irrelevant.sh
   # populating relevant csv
-  cd /home/ze/Desktop/mining_results/${i}_results/${i}/
-  get_relevant_csv > /home/ze/Desktop/mining_results/relevant.csv
-  cd /home/ze/miningframework/
+  cd "$results_path"/${i}_results/${i}/
+  get_relevant_csv > "$results_path"/relevant.csv
+  cd "$miningframework_path"/
 done
 
 
