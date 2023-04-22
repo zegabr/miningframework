fa81088dfcb426e6bbecca05540d81464e5ccb52
zegabr <jgsp2@cin.ufpe.br>
2023-04-22 14:52:26 -0300

    update test.sh

--- test.sh
+++ test.sh
@@ -2,13 +2,9 @@
 
 ./gradlew assemble
 
-# names=(matplotlib tensorflow certbot flask ipython requests salt scrapy sentry tornado)
-names=(matplotlib)
+names=(matplotlib tensorflow certbot flask ipython requests salt scrapy sentry tornado)
 miningframework_path=$(pwd)
-# results_path="$miningframework_path/mining_results_awk_optimization"
-# results_path="$miningframework_path/mining_results_awk_optimization_no_colon"
-# results_path="$miningframework_path/mining_results_indentation"
-results_path="$miningframework_path/mining_results_speedtest"
+results_path="$miningframework_path/mining_results_indentation"
 
 echo "deleting all_results"
 rm "$results_path"/all_results.csv
@@ -42,35 +38,28 @@ do
     echo "running mining framework"
     time ./gradlew run --args="-e .py -i injectors.CSDiffModule -l '( ) , :' -t 1 -s 01/01/2021 -u 03/01/2021 ./projects/${i}.csv "$results_path"/${i}_results"
 
-    # if [[ -e "$results_path"/${i}_results/results.csv ]]; then
-    #     if [[ ! -e "$results_path"/all_results.csv ]]; then
-    #         echo "created all_results and relevant csvs"
-    #         head -n 1 "$results_path"/${i}_results/results.csv > "$results_path"/all_results.csv
-    #         head -n 1 "$results_path"/${i}_results/results.csv > "$results_path"/relevant.csv
-    #     fi
-    #     grep -e "true" -e "false" "$results_path"/${i}_results/results.csv >> "$results_path"/all_results.csv
-    # fi
-
-    # re_running csdiff v3 to see check for mistakes
-    # if [ "$(ls -A "$results_path" | wc -l)" -gt 1 ]; then
-    #     cd "$results_path"/${i}_results/
-    #     echo "running csdiff again for the missing folders"
-    #     bash "$miningframework_path"/re_run_csdiff.sh
-    # fi
+    if [[ -e "$results_path"/${i}_results/results.csv ]]; then
+        if [[ ! -e "$results_path"/all_results.csv ]]; then
+            echo "created all_results and relevant csvs"
+            head -n 1 "$results_path"/${i}_results/results.csv > "$results_path"/all_results.csv
+            head -n 1 "$results_path"/${i}_results/results.csv > "$results_path"/relevant.csv
+        fi
+        grep -e "true" -e "false" "$results_path"/${i}_results/results.csv >> "$results_path"/all_results.csv
+    fi
 
     # clearing miningframework irrelevant cases (csdiff == diff3)
-    # if [ -d "$results_path"/${i}_results/ ]; then
-    #     cd "$results_path"/${i}_results/
-    #     echo "deleting diff3 == csdiff folders"
-    #     bash "$miningframework_path"/clear_irrelevant.sh
-    # fi
+    if [ -d "$results_path"/${i}_results/ ]; then
+        cd "$results_path"/${i}_results/
+        echo "deleting diff3 == csdiff folders"
+        bash "$miningframework_path"/clear_irrelevant.sh
+    fi
 
     # populating relevant csv
-    # if [ -d "$results_path"/${i}_results/${i}/ ]; then
-    #   cd "$results_path"/${i}_results/${i}/
-    #   echo "populating relevant.csv with only folders where csdiff != diff3"
-    #   get_relevant_csv >> "$results_path"/relevant.csv
-    # fi
+    if [ -d "$results_path"/${i}_results/${i}/ ]; then
+      cd "$results_path"/${i}_results/${i}/
+      echo "populating relevant.csv with only folders where csdiff != diff3"
+      get_relevant_csv >> "$results_path"/relevant.csv
+    fi
 done
 
 
